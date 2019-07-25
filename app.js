71cfc779e97a9e0727b16c551a0d387d25448866
zlx <zlx@omk.io>
2019-07-25 17:45:32 +0800

    fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+var execSync = require('child_process').execSync;
 const server = app.listen(process.env.PORT || 8090, function() {
   var host = server.address().address;
   var port = server.address().port;
@@ -11,11 +11,11 @@ const server = app.listen(process.env.PORT || 8090, function() {
 app.use(bodyParser.urlencoded({extend:false}));
 app.use(bodyParser.json());
 app.use('/issues/new', function(req, res) {
-  console.log('有新的issue 👏');
+  console.log('有新的issue 👏', req.body.payload.action);
   try {
     var payload = JSON.parse(req.body.payload)
     if (payload.action === 'opened' || payload.action === 'deleted') {
-      require('./createReadme')
+      execSync("node ./createReadme.js")
     }
   } catch (err) {
     console.log('some error has happend:', err)
