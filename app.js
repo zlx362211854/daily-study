62058b4f9f7a95a56f52be86f2410f8ab4beaae3
zlx <zlx@omk.io>
2019-07-25 17:26:01 +0800

    fix body

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 const server = app.listen(process.env.PORT || 8090, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('github hooks app is listening at http://%s:%s', host, port);
 });
+ 
+app.use(bodyParser.urlencoded({extend:false}));
+app.use(bodyParser.json());
 app.use('/issues/new', function(req, res) {
-  console.log('有新的issue', req.body)
+  console.log('有新的issue', req.body);
   try {
     if (req.body.action === 'opened') {
       require('./createReadme')
