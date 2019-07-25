a65553776181f1e395860f159141985f4706660e
zlx <zlx@omk.io>
2019-07-25 17:12:37 +0800

    fix

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
-const server = app.listen(PORT || 8090, function() {
+const server = app.listen(process.env.PORT || 8090, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('github hooks app is listening at http://%s:%s', host, port);
 });
-app.get('/issues/new', function(req, res) {
-  console.log('有新的issue')
-  require('./createReadme')
+app.use('/issues/new', function(req, res) {
+  console.log('有新的issue', req.body)
+  try {
+    if (req.body.action === 'opened') {
+      require('./createReadme')
+    }
+  } catch (err) {
+    console.log('some error has happend:', err)
+  }
 });
