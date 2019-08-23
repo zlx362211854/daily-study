4387f406fd510f7c3b2a1c05fb2fc505d3a6132a
zlx <zlx@omk.io>
2019-08-23 11:42:09 +0800

    remove

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const server = app.listen(process.env.PORT || 8090, function() {
   var port = server.address().port;
   console.log('github hooks app is listening at http://%s:%s', host, port);
 });
-
 app.use(bodyParser.urlencoded({ extend: false }));
 app.use(bodyParser.json());
 app.use('/issues/new', function(req, res) {
@@ -16,26 +15,6 @@ app.use('/issues/new', function(req, res) {
   try {
     var payload = JSON.parse(req.body.payload);
     if (payload.action === 'opened' || payload.action === 'deleted') {
-      if (payload.action === 'opened') {
-        request.post({
-          headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36 SE 2.X MetaSr 1.0'
-          },
-          url: 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=b23250ea-d4b1-4060-8322-ad6781898cd5',
-          form: {
-            msgtype: 'markdown',
-            markdown: {
-              "content": `> ${payload.sender.login} created a new issue \r\n\r\n [Click here for details](${payload.issue.url})`
-            }
-          },
-          function(error, response, body) {
-            if (error) {console.log('error:', error)}
-            if (!error && response.statusCode == 200 && body) {
-              console.log('successful send messgae to wechat');
-            }
-          }
-        });
-      }
       execSync('node ./createReadme.js');
     }
   } catch (err) {
